b3d8ea89c9b100ba7581cb5f78f121e956a9b66e
Ed Solis <edsfocci@gmail.com>
2015-05-15 13:47:49 -0500

    Minor submitSignupForm() bugfixes

--- assets/js/public/signup/SignupController.js
+++ assets/js/public/signup/SignupController.js
@@ -24,12 +24,16 @@ angular.module('SignupModule').controller('SignupController', ['$scope', '$http'
 
 		// Handle known error type(s).
 		// If using sails-disk adpater -- Handle Duplicate Key
-		var emailAddressAlreadyInUse = sailsResponse.status == 409;
+		var emailAddressAlreadyInUse = sailsResponse.status === 409;
 
 		if (emailAddressAlreadyInUse) {
 			toastr.error('That email address has already been taken, please try again.', 'Error');
 			return;
 		}
+
+		// Handle unknown errors.
+		toastr.error('An unexpected error occurred, please try again.', 'Error');
+		return;
 
 		})
 		.finally(function eitherWay(){
